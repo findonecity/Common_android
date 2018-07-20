7bfedd3d88659124a4bdf77e9b880c1e9acbbdb7
zj <420361460@qq,com>
2018-07-20 16:16:26 +0800

    改动了现有的两个界面

--- app/src/main/java/com/example/zj/common_android/recharge/RechargeFragment.kt
+++ app/src/main/java/com/example/zj/common_android/recharge/RechargeFragment.kt
@@ -3,6 +3,7 @@ package com.example.zj.common_android.recharge
 import com.example.zj.common_android.R
 import com.example.zj.common_android.base.BaseContract
 import com.example.zj.common_android.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_recharge.*
 
 /**
  * Created by zj on 2018/7/9.
